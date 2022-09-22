46fb94e6e8db2b73daf70c308bf40c86facddf91
druhy_rocnik <andrejpuchon@gmail.com>
2022-09-22 09:30:53 +0200

    Upload

--- src/Hodina1.java
+++ src/Hodina1.java
@@ -1,13 +1,22 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Hodina1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Napíš niečo");
-        int a = scanner.nextInt();
-        System.out.println(a);
-        System.out.println(a);
-        System.out.println(a);
-
+        int[] pole = new int[10];
+        int low=0;
+        int cislo = 0;
+        Random r = new Random(10);
+        for(int i = 0; i< pole.length;i++){
+            pole[i] =r.nextInt(100);
+            System.out.println(pole[i]);
+        }
+        for(int j=0; j< pole.length;j++){
+            if(pole[j]<low){
+                low = pole[j];
+                cislo =j;
+            }
+        }
+        System.out.println(low+"na pozici"+cislo);
     }
 }
---
+++ src/Hodina2.java
@@ -0,0 +1,125 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Hodina2 {
+    public static void main(String[] args) {
+        //úloha 1
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Napíš číslo");
+//        int a = scanner.nextInt();
+//        int prvé=20;
+//        int druhé=30;
+//        if(a>prvé && a<druhé){
+//            System.out.println(a+"patrí do rozmedzia 20-30");
+//        }else if(a<prvé){
+//            System.out.println(a+"Nepatrí je menšie");
+//        }else if(a>druhé){
+//            System.out.println(a+"Nepatrí je väčšie");
+//        }
+
+        //úloha2
+//        for(int i = 0; i<=100; i++){
+//            System.out.println(i);
+//            if(i%2==0){
+//                System.out.println(i+" Je párne");
+//            }else{
+//                System.out.println(i+" Je nepárne");
+//            }
+//        }
+
+        //úloha3
+//        char a ='-';
+//        for(int i =0; i<10; i++){
+//            if(i>0){
+//                for(int o =1; o<=i; o++){
+//                    System.out.print(a);
+//                }
+//            }
+//            System.out.print(i+""+i);
+//            if(i>0);{
+//                for(int j=1; j<=i;j++){
+//                    System.out.print(a);
+//                }
+//            }
+//            System.out.println();
+//        }
+
+        //úloha4
+//        boolean výpis = true;
+//        while(výpis){
+//            for(int i=0;i<=5;i++){
+//                System.out.println("číslo"+i);
+//            }
+//            výpis = false;
+//        }
+
+        //úloha5
+//        String[][] pole =new String[5][5];
+//        for(int i =0;i< pole.length;i++){
+//            for(int j =0;j<pole.length;j++){
+//                pole[i][j]="Empty";
+//            }
+//        }
+//        pole[1][4]="Ano";
+//        pole[0][2]="Ano";
+//        pole[3][0]="Ano";
+//        for(int i =0;i< pole.length;i++){
+//            for(int j =0;j< pole.length;j++){
+//                System.out.print(pole[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
+
+        //úloha6
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        int výsledok = a+b;
+//        System.out.println(výsledok);
+
+        //úloha7
+//        String retazec = "Ako sa máš";
+//        int počet = scanner.nextInt();
+//        for(int i=0; i<počet;i++){
+//            System.out.println(retazec);
+//        }
+
+        //úloha8
+        //            public static String beza (Scanner scan){
+//        String veta = scan.nextLine();
+//        String[] pole = new String[];
+//        for (int i = 0; i < pole.length; i++){
+//            if ()
+//        }
+//    }
+        //uloha 9
+//        int[] pole = new int[10];
+//        for (int i = 0; i< pole.length; i++){
+//            pole[i] = ran.nextInt(10)+1;
+//        }
+//        for (int o = 0; o< pole.length; o++){
+//            System.out.print(pole[o]);
+//            if (pole[o] % 2 == 0){
+//                System.out.print(" Parne");
+//            } else{
+//                System.out.print(" Neparne");
+//            }
+//            if (pole[o] % 3 == 0){
+//                System.out.print(", delitelne 3");
+//            } else{
+//                System.out.print(", nedelitelne 3");
+//            }
+//            System.out.println();
+//        }
+        //uloha 10
+//        int cislo = 6;
+//    }
+//    public static String parnost(int cislo){
+//        if (cislo % 2 == 0) {
+//            return "Parne";
+//        } else {
+//            return "Neparne";
+//        }
+    }
+    }
+
+
